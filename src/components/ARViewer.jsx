2d08c00067b3b26629d73bb9d1f82c662da84cac
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-07-08 18:39:42 +0530

    changine

--- src/components/ARViewer.jsx
+++ src/components/ARViewer.jsx
@@ -1,14 +1,8 @@
 // src/components/ARViewer.jsx
-import React, { useEffect } from "react";
+import React from "react";
 import "@google/model-viewer";
 
 const ARViewer = ({ url }) => {
-  useEffect(() => {
-    const viewer = document.querySelector("model-viewer");
-    if (viewer) {
-      viewer.style.setProperty("--initial-scale", "0.16"); // Set initial scale to 16% of viewport width
-    }
-  }, []);
   return (
     <model-viewer
       src={url}

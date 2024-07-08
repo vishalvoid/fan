cf8fa87f40ccd4cee9dba1fb7f33560f59e76eec
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-07-08 18:06:28 +0530

    changing

--- src/components/ARViewer.jsx
+++ src/components/ARViewer.jsx
@@ -1,35 +1,16 @@
 // src/components/ARViewer.jsx
 import React from "react";
 import "@google/model-viewer";
-import { useEffect, useRef } from "react";
 
 const ARViewer = ({ url }) => {
-  const viewerRef = useRef(null);
-
   useEffect(() => {
-    const viewer = viewerRef.current;
-
-    // Listen for when AR mode is entered
-    viewer.addEventListener("ar-mode-activated", () => {
-      // Reduce the scale when entering AR mode
-      viewer.setAttribute("scale", "0.2 0.2 0.2"); // Adjust this scale factor as needed
-    });
-
-    // Listen for when AR mode is exited
-    viewer.addEventListener("ar-mode-deactivated", () => {
-      // Reset the scale when exiting AR mode
-      viewer.setAttribute("scale", "1 1 1");
-    });
-
-    // Cleanup event listeners
-    return () => {
-      viewer.removeEventListener("ar-mode-activated");
-      viewer.removeEventListener("ar-mode-deactivated");
-    };
+    const viewer = document.querySelector("model-viewer");
+    if (viewer) {
+      viewer.style.setProperty("--initial-scale", "0.16"); // Set initial scale to 16% of viewport width
+    }
   }, []);
   return (
     <model-viewer
-      ref={viewerRef}
       src={url}
       ar
       ar-modes="scene-viewer quick-look webxr"

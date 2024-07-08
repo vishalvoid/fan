d0002c0ac75be973e2cd7db2463131297ad22a38
Vishal Kumar Singh <tech.vishalkrsingh@gmail.com>
2024-07-08 17:56:37 +0530

    changing to fix for 16%

--- src/components/ARViewer.jsx
+++ src/components/ARViewer.jsx
@@ -1,13 +1,37 @@
 // src/components/ARViewer.jsx
 import React from "react";
 import "@google/model-viewer";
+import { useEffect, useRef } from "react";
 
 const ARViewer = ({ url }) => {
+  const viewerRef = useRef(null);
+
+  useEffect(() => {
+    const viewer = viewerRef.current;
+
+    // Listen for when AR mode is entered
+    viewer.addEventListener("ar-mode-activated", () => {
+      // Reduce the scale when entering AR mode
+      viewer.setAttribute("scale", "0.2 0.2 0.2"); // Adjust this scale factor as needed
+    });
+
+    // Listen for when AR mode is exited
+    viewer.addEventListener("ar-mode-deactivated", () => {
+      // Reset the scale when exiting AR mode
+      viewer.setAttribute("scale", "1 1 1");
+    });
+
+    // Cleanup event listeners
+    return () => {
+      viewer.removeEventListener("ar-mode-activated");
+      viewer.removeEventListener("ar-mode-deactivated");
+    };
+  }, []);
   return (
     <model-viewer
+      ref={viewerRef}
       src={url}
       ar
-      ar-scale="16%"
       ar-modes="scene-viewer quick-look webxr"
       auto-rotate
       camera-controls
